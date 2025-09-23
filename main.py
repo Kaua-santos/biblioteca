38bfebf9026a658af4ad60fb72692bbfacefd3a6
kaua santos <kaua7santos7oliveiraa@gmail.com>
2025-09-23 09:53:11 -0300

    etapa 1 - criação do banco e tabela

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 # estrutura do projeto 
+# etapa 1 do projeto 
+import sqlite3
+import _sqlite3
+# etapa 1
+conexao = sqlite3.connect("biblioteca.db")
+cursor = conexao.cursor()
+
+# Criando uma tabela no banco chamada de "biblioteca"
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS livros (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     autor TEXT NOT NULL,
+     ano INTEGER,
+     disponivel TEXT    
+     )                
+ """)
+print("Tabela criada com sucesso!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
